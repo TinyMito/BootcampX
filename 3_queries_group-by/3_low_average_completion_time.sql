282d137a3c72d90bdbe3460ed31f56852fc21a85
TinyMito <k.ip@me.com>
2023-06-05 23:21:39 -0400

    BootcampX Queries 3 - Group By

--- 3_queries_group-by/3_low_average_completion_time.sql
+++ 3_queries_group-by/3_low_average_completion_time.sql
@@ -1,6 +1,7 @@
-SELECT students.name AS student, AVG(asg.duration) AS average_assignment_duration
+SELECT students.name AS student, AVG(asgsub.duration) AS average_assignment_duration, AVG(asg.duration) AS average_estimated_duration
 FROM students
-JOIN assignment_submissions asg ON asg.student_id = students.id
+JOIN assignment_submissions asgsub ON asgsub.student_id = students.id
+JOIN assignments asg ON asg.id = asgsub.assignment_id
 GROUP BY students.name, students.end_date
-HAVING students.end_date IS NULL
-ORDER BY 2 desc;
+HAVING students.end_date IS NULL AND AVG(asgsub.duration) <= AVG(asg.duration)
+ORDER BY 2 asc;
