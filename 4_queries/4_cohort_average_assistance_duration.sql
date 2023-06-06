47ebd7b86f3dc8f5c846429256e9713d37d5d8a1
TinyMito <k.ip@me.com>
2023-06-06 00:19:57 -0400

    BootcampX Queries 4

---
+++ 4_queries/4_cohort_average_assistance_duration.sql
@@ -0,0 +1,9 @@
+SELECT AVG(result.total_duration) AS average_total_duration
+FROM (
+  SELECT cohorts.name AS cohort, SUM(asr.completed_at - asr.started_at) AS total_duration
+  FROM assistance_requests asr
+  JOIN students ON students.id = asr.student_id
+  JOIN cohorts ON cohorts.id = students.cohort_id
+  GROUP BY cohorts.name
+  ORDER BY total_duration
+) AS result;
