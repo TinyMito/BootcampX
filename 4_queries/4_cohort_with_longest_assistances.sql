e3338ca9dd6789835d1a49fdbb9936a13ee65cc8
TinyMito <k.ip@me.com>
2023-06-06 00:09:31 -0400

    BootcampX Queries 4

---
+++ 4_queries/4_cohort_with_longest_assistances.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name AS name, AVG(asr.completed_at - asr.started_at) AS average_assistance_time
+FROM assistance_requests asr
+JOIN students ON students.id = asr.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time DESC
+LIMIT 1;
