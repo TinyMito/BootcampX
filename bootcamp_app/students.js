10f994f808216971b150fcab0d5b350cc855c653
TinyMito <k.ip@me.com>
2023-06-14 23:57:09 -0400

    Update to include cohort name

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,14 +8,15 @@ const pool = new Pool({
 });
 
 pool.query(`
-SELECT id, name, cohort_id
+SELECT students.id AS student_id, students.name AS student_name, cohorts.name AS cohort_name
 FROM students
+JOIN cohorts ON students.cohort_id = cohorts.id
 LIMIT 5;
 `)
 .then(res => {
   //console.log(res.rows);
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
+    console.log(`${user.student_name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`);
   })
 })
 .catch(err => console.error('query error', err.stack));
