a2027c9d846d7a742b5b44830060407c7bdea353
TinyMito <k.ip@me.com>
2023-06-15 00:11:38 -0400

    Add SQL teachers.js

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const args = process.argv.slice(2);
+
+pool.query(`
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE '%${args[0]}%'
+ORDER BY teachers.name;
+`)
+.then(res => {
+  //console.log(res.rows);
+  console.log('connected');
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
